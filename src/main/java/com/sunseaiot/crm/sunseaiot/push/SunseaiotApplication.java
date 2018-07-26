1395eb89015afb8bcf83c070bde2061dadb5b3c1
cjb-github <cjb1mc@163.com>
2018-07-26 14:00:53 +0800

    sunsea测试提交

---
+++ src/main/java/com/sunseaiot/crm/sunseaiot/push/SunseaiotApplication.java
@@ -0,0 +1,12 @@
+package com.sunseaiot.crm.sunseaiot.push;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunseaiotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunseaiotApplication.class, args);
+	}
+}
